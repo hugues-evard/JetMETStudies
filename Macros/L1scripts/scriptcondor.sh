1b8814af47472039f4056d4f8cb46ffc4cc8b8fc
Hugues EVARD <hugues.evard@cern.ch>
2022-09-15 16:40:10 +0200

    Modified path of input/output files

--- Macros/L1scripts/scriptcondor.sh
+++ Macros/L1scripts/scriptcondor.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cd /user/lathomas/L1Studies/SampleGeneration/SingleNeutrinoPU1/CMSSW_12_2_1/src
+#cd /user/lathomas/L1Studies/SampleGeneration/SingleNeutrinoPU1/CMSSW_12_2_1/src
+cd /user/hevard/CMSSW_12_4_8/src
 cmsenv 
-cd /user/lathomas/MacrosforM/L1Studies/L1NtuplesAnalysis/SandBox
+#cd /user/lathomas/MacrosforM/L1Studies/L1NtuplesAnalysis/SandBox
+cd /user/hevard/CMSSW_12_4_8/src/JetMETStudies/Macros/L1scripts
 
 python3 analyzel1.py --maxEvents -1 -i $1 -o $2 -c $3
 
